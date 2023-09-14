4a564e172a4cce033bd39fa47398725d769ab4c9
J <32039944+jhuang265@users.noreply.github.com>
2023-09-13 21:02:51 -0400

    Updates

--- jobs/modified_roberta_base_mlm-start.sh
+++ jobs/modified_roberta_base_mlm-start.sh
@@ -36,7 +36,7 @@ echo -e "$LSB_JOBID" >> $TRAIN_PATH/info/$LSB_JOBNAME.info
 
 # Run
 python $TRAIN_PATH/deepy.py $TRAIN_PATH/train.py --conf_dir $TRAIN_PATH/configs_mlm \
-	setup/setup_roberta_base_resume.yml \
+	setup/setup_modified_roberta_base_resume.yml \
 	modified_roberta/modified_roberta_base.yml \
 	datasets_ben/val/pile_slimp.yml \
 	datasets_ben/train/slim_pajama_606B.yml \
