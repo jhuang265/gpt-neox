4705493385affd93b7ce55e742546db1ffae431a
J <32039944+jhuang265@users.noreply.github.com>
2023-09-27 09:36:24 -0400

    Add with_token debug

---
+++ jobs/debug_roberta_base_mlm_with_tokens-short.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#BSUB -nnodes 1
+#BSUB -W 0:30
+#BSUB -q batch
+#BSUB -o /gpfs/alpine/csc499/scratch/jerry.huang/gpt-neox/logs/debug_with_tokens_mlm-%J.out
+#BSUB -e /gpfs/alpine/csc499/scratch/jerry.huang/gpt-neox/logs/debug_with_tokens_mlm-%J.err
+#BSUB -J debug_with_tokens_mlm
+#BSUB -alloc_flags gpudefault
+#BSUB -P CSC499
+#BSUB -N jerry.huang@mila.quebec
+#BSUB -B jerry.huang@mila.quebec
+
+# Set up the environment
+source ~/.bashrc
+source /gpfs/alpine/csc499/scratch/jerry.huang/setup.sh
+
+# Activate conda environment
+# source /gpfs/alpine/csc499/scratch/jerry.huang/miniconda3/etc/profile.d/conda.sh
+conda activate gpt-neox
+which python
+
+export TORCH_EXTENSIONS_DIR=/gpfs/alpine/csc499/scratch/jerry.huang/latest_install/cache
+
+# Move to the gpt-neox install
+export TRAIN_PATH=/gpfs/alpine/csc499/scratch/jerry.huang/gpt-neox
+cd $TRAIN_PATH
+
+# Write the hostfile for this job
+bash /gpfs/alpine/csc499/scratch/jerry.huang/write_hostfile.sh
+export DLTS_HOSTFILE=/gpfs/alpine/csc499/scratch/jerry.huang/hostfiles/$LSB_JOBID-hosts
+
+python $TRAIN_PATH/deepy.py $TRAIN_PATH/train.py --conf_dir $TRAIN_PATH/configs_mlm setup/setup_debug_with_tokens.yml debug/debug_base_with_tokens.yml datasets_ben/val/slimp.yml datasets_ben/train/slim_pajama_606B.yml load_ben/none.yml 
